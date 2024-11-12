ccfb78f242c9445d07821133d8b62a2c439ac77a
Amy Cox <amy_cox@me.com>
2024-11-12 19:06:43 +0000

    job list working

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,11 @@
-document
-  .getElementById('visitPxlPro')
-  .addEventListener('click', function (event) {
-    event.preventDefault();
-    console.log('fake link to pixlPro');
-  });
+// document
+//   .getElementById('visitPxlPro')
+//   .addEventListener('click', function (event) {
+//     event.preventDefault();
+//     console.log('fake link to pixlPro');
+//   });
 
-document.getElementById('signin').addEventListener('click', function (event) {
-  event.preventDefault();
-  console.log('Would potentially open modal to signin');
-});
+// document.getElementById('signin').addEventListener('click', function (event) {
+//   event.preventDefault();
+//   console.log('Would potentially open modal to signin');
+// });
