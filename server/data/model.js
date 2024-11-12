014e5ca6e5b17d7c21e01d85147523fe87574d30
Amy Cox <amy_cox@me.com>
2024-11-12 16:23:50 +0000

    added nav Items to model.js

--- server/data/model.js
+++ server/data/model.js
@@ -4,21 +4,45 @@ module.exports = {
   appVersion: pkg.version,
   assetPath: '/public',
   pageTitle: 'PxlPro Jobs',
+  heroMessage: 'Search 1,570 jobs.',
   navItems: [
     {
-      label: 'Home',
-      href: '#'
+      label: "Home",
+      href: "#",
+      id: "home",
+      arialLabel: "Navigate to home page"
     },
     {
-      label: 'Find a job',
-      href: '#find-a-job'
+      label: "Find a job",
+      href: "#find-a-job",
+      id: "find-a-job",
+      ariaLabel: "Navigate to job listings"
     },
     {
-      label: 'Careers advice',
-      href: '#careers'
-    }
+      label: "Careers advice",
+      href: "#careers",
+      id: "careers",
+      ariaLabel: "Navigate to our careers advice page"
+    },
+    {
+      label: "Job alerts",
+      href: "#job-alerts",
+      id: "job-alerts",
+      ariaLabel: "Sign up to receive job alerts"
+    },
+    {
+      label: "Your jobs",
+      href: "#your-jobs",
+      id: "your-jobs",
+      ariaLabel: "Navigate to your saved jobs"
+    },
+    {
+      label: "Advertise a job",
+      href: "#advertise-a-job",
+      id: "advertise",
+      ariaLabel: "Advertise a job"
+    },
   ],
-  heroMessage: 'Search 1,570 jobs.',
   ctas: [
     {
       icon: 'job-alerts',
